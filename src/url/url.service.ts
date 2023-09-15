15421bec18899e2f6025078a60a194019b4c103f
MurAd <muradisayevazer@gmail.com>
2023-09-15 13:26:08 +0400

    url methods completed

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUrlDto } from './dto/create-url.dto';
-import { UpdateUrlDto } from './dto/update-url.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { ShortenUrlDto } from './dto/shorten-url.dto';
+import { Url } from './entities/url.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class UrlService {
-  create(createUrlDto: CreateUrlDto) {
-    return 'This action adds a new url';
+  constructor(@InjectRepository(Url) private urlRepository: Repository<Url>) {}
+  async shortenUrl(shortenUrlDto: ShortenUrlDto) {
+    const url = await this.urlRepository.findOneBy({
+      longUrl: shortenUrlDto.longUrl,
+    });
+
+    if (url) throw new BadRequestException('Url already exists');
+
+    const urlKey: string = nanoid(5);
+    const baseUrl = 'http://localhost:3000';
+    const shortUrl = `${baseUrl}/${urlKey}`;
+
+    try {
+      const url = this.urlRepository.create({
+        urlKey,
+        longUrl: shortenUrlDto.longUrl,
+        shortUrl,
+      });
+      await this.urlRepository.save(url);
+
+      return shortUrl;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
   }
 
-  findAll() {
-    return `This action returns all url`;
-  }
+  async redirect(urlKey: string) {
+    const url = await this.urlRepository.findOneBy({
+      urlKey,
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} url`;
-  }
+    if (!url) throw new BadRequestException('Url does not exist');
 
-  update(id: number, updateUrlDto: UpdateUrlDto) {
-    return `This action updates a #${id} url`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} url`;
+    return url.longUrl;
   }
 }
